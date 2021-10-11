93bee6698f409c3e988776c836b8f3046ece1b21
andreasLojdahl <andreas.lojdahl@gmail.com>
2021-10-11 19:12:34 +0200

    Test commit

--- hiq-coding-test/WeatherForecast.cs
+++ hiq-coding-test/WeatherForecast.cs
@@ -5,7 +5,7 @@ namespace hiq_coding_test
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
-
+        //test
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
