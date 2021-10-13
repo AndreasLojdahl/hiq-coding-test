9e753f7a66c950d333903b2f3156b65d5a1508bc
andreasLojdahl <andreas.lojdahl@gmail.com>
2021-10-13 22:07:21 +0200

    Added fileUploadController with post endpoint

---
+++ hiq-coding-test/Controllers/FileUploadController.cs
@@ -0,0 +1,57 @@
+﻿using hiq_coding_test.Models;
+using hiq_coding_test.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hiq_coding_test.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class FileUploadController: ControllerBase
+    {
+        private readonly IFileUploadService _fileUploadService;
+
+        public FileUploadController(IFileUploadService fileUploadService)
+        {
+            _fileUploadService = fileUploadService;
+        }
+
+        [HttpPost("fileUpload")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [Consumes("multipart/form-data")]
+        public IActionResult UploadFile([FromForm] IFormFile file)
+        {
+         
+            {
+                var isAcceptedFile = _fileUploadService.CheckIfAcceptedFileType(file);
+                Console.WriteLine(isAcceptedFile);
+                Console.WriteLine(file.FileName);
+                if (isAcceptedFile)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return new StatusCodeResult(StatusCodes.Status400BadRequest);
+                }
+            }
+            try
+            {
+                return null;
+                
+            }
+            catch
+            {
+                return new StatusCodeResult(StatusCodes.Status400BadRequest);
+
+            }
+        }
+
+  
+    }
+}
