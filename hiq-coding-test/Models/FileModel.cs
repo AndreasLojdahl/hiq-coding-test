f1769c572e9457fe8ea82616ddd9e4321f1a7b62
andreasLojdahl <andreas.lojdahl@gmail.com>
2021-10-15 21:10:36 +0200

    added filemodel class

---
+++ hiq-coding-test/Models/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hiq_coding_test.Models
+{
+    public class FileModel
+    {
+        public int count { get; set; }
+        public string text { get; set; }
+    }
+}
